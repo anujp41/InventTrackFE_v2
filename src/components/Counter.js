bee0a7dcbc03a6e143672d70e15aa854b0120bb0
anujp41 <anuj.pant41@gmail.com>
2019-01-14 12:40:49 -0500

    update FE using contentEditable

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,38 +5,86 @@ import './Counter.css';
 class Counter extends React.Component {
   constructor() {
     super();
+    // this.fruitNameRef = React.createRef();
+    this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.updateState = this.updateState.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
+    this.htmlItem = this.htmlItem.bind(this);
     this.state = {
-      counter: 0
+      counter: 0,
+      url: '',
+      editMe: true,
+      apiData: []
     };
   }
   componentDidMount() {
+    const url =
+      process.env.NODE_ENV === 'development'
+        ? process.env.REACT_APP_DEV_URL
+        : process.env.REACT_APP_PROD_URL;
     axios
-      .get('/data')
+      .get(`${url}/data`)
       .then(this.updateState)
+      .then(() => this.setState({ url }))
       .catch(err => console.log('error ', err));
   }
   updateState(response) {
-    this.setState({ counter: response.data.apples });
+    this.setState({ apiData: response.data });
   }
-  handleClick(toDo) {
-    return toDo === '+'
-      ? axios.post('/data').then(this.updateState)
-      : axios.delete('/data').then(this.updateState);
+  handleChange(id) {
+    console.log('updated name: ', this.refs[`fruit${id}`].textContent);
+  }
+  handleClick(itemId, toDo) {
+    axios
+      .get(`${this.state.url}/data/${toDo}/${itemId}`)
+      .then(this.updateState);
+  }
+  handleEnter(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      console.log('stopped new line! ', this.state.apiData);
+    }
+    // console.log(event.keyCode);
+  }
+  htmlItem(item) {
+    return { __html: item };
   }
   render() {
+    const { apiData } = this.state;
+    console.log(apiData);
     return (
       <div className="container">
-        <section>
-          <button className="decrease" onClick={() => this.handleClick('-')}>
-            -
-          </button>
-          <button className="increase" onClick={() => this.handleClick('+')}>
-            +
-          </button>
-        </section>
-        <div className="count">Counter: {this.state.counter}</div>
+        {apiData.length > 0 &&
+          apiData.map(item => (
+            <div key={item.fruit} className="fruit-container">
+              <div className="count">
+                <p
+                  ref={'fruit' + item.id}
+                  className="inline"
+                  contentEditable={true}
+                  onKeyDown={this.handleEnter}
+                  onInput={() => this.handleChange(item.id)}
+                  dangerouslySetInnerHTML={{ __html: item.fruit }}
+                />{' '}
+                : <p className="inline">{item.count}</p>
+              </div>
+              <section>
+                <button
+                  className="decrease"
+                  onClick={() => this.handleClick(item.id, 'subtract')}
+                >
+                  -
+                </button>
+                <button
+                  className="increase"
+                  onClick={() => this.handleClick(item.id, 'add')}
+                >
+                  +
+                </button>
+              </section>
+            </div>
+          ))}
       </div>
     );
   }
