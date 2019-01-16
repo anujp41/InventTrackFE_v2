bd68afacb2204a654fc89aa49816f6be8aaf6eca
anujp41 <anuj.pant41@gmail.com>
2019-01-16 14:12:36 -0500

    added socket

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Counter, Wrapper } from './components';
+import socketIOClient from 'socket.io-client';
 
 class App extends Component {
+  componentDidMount() {
+    const socket = socketIOClient('http://192.168.86.209:8000');
+    socket.on('news', data => {
+      console.log('socket data', data);
+      socket.emit('browser', 'Hello from browser my friend!');
+    });
+  }
   render() {
     return (
       <Wrapper>
