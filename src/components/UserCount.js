6ad7d382f14cec7e8688a1d0cb54975d93592b07
anujp41 <anuj.pant41@gmail.com>
2019-02-09 23:39:32 -0500

    updating UserCount component to add fruits to user

--- src/components/UserCount.js
+++ src/components/UserCount.js
@@ -5,10 +5,12 @@ import './UserCount.css';
 class UserCount extends React.Component {
   constructor() {
     super();
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       userData: []
     };
   }
+
   componentDidMount() {
     const url =
       process.env.NODE_ENV === 'development'
@@ -16,39 +18,63 @@ class UserCount extends React.Component {
         : process.env.REACT_APP_PROD_URL;
     axios
       .get(`${url}/data/user`)
-      .then(response => this.setState({ userData: response.data }));
+      .then(response => this.setState({ userData: response.data, url }));
     // .then(this.updateState)
     // .then(() => this.setState({ url }))
     // .catch(err => console.log('error ', err));
   }
+
+  handleClick(userId, fruitId) {
+    axios
+      .put(`${this.state.url}/data/user/`, { userId, fruitId })
+      .then(response => console.log('response ', response));
+  }
+
   render() {
     const { userData } = this.state;
-    userData.length && console.log(userData);
-    userData[0] &&
-      console.log('here ', userData[0] && userData[0].Consumer.length);
     return (
       <>
         <div className="header">User & Fruits</div>
         <div className="col-container">
           {userData.length &&
-            userData.map(userInfo => {
-              console.log('userInfo ', userInfo.Consumer);
-              return (
-                <div className="table-grid">
-                  <span key={userInfo.id + userInfo.name}>{userInfo.name}</span>
-                  {userInfo.Consumer.length ? (
-                    userInfo.Consumer.map(fruitInfo => (
-                      <>
-                        <span>{fruitInfo.name}</span>
-                        <span>{fruitInfo.UserFruit.counter}</span>
-                      </>
-                    ))
-                  ) : (
-                    <span className="none">None</span>
-                  )}
-                </div>
-              );
-            })}
+            userData.map(userInfo => (
+              <div className="table-grid" key={userInfo.name}>
+                <span key={userInfo.id + userInfo.name}>{userInfo.name}</span>
+                {userInfo.Consumer.length ? (
+                  userInfo.Consumer.map(fruitInfo => (
+                    <React.Fragment
+                      key={userInfo.id + userInfo.name + fruitInfo.name}
+                    >
+                      <span
+                        key={userInfo.id + userInfo.name + fruitInfo.id}
+                        onClick={() =>
+                          this.handleClick(userInfo.id, fruitInfo.id)
+                        }
+                      >
+                        {fruitInfo.name}
+                      </span>
+                      <span
+                        key={
+                          userInfo.id +
+                          userInfo.name +
+                          fruitInfo.id +
+                          fruitInfo.counter
+                        }
+                      >
+                        {fruitInfo.UserFruit.counter}
+                      </span>
+                    </React.Fragment>
+                  ))
+                ) : (
+                  <span
+                    className="none"
+                    key={userInfo.id + userInfo.name + userInfo.id}
+                  >
+                    None
+                  </span>
+                )}
+              </div>
+            ))}
         </div>
       </>
     );
